e08b25278f235f21f6e61ab829178d5adcca8dbf
Joost Soeterbroek <joost.soeterbroek@gmail.com>
2022-07-07 14:23:26 +0200

    sync 20220707-142318

--- org-mode/sync.sh
+++ org-mode/sync.sh
@@ -4,10 +4,10 @@
 DATETIME=`date "+%Y%m%d-%H%M%S"`
 
 echo "INFO: ${DATETIME} sync from dropbox to local org files..."
-rsync -anv ${HOME}/Dropbox/org ${HOME}/org/
+rsync -avz ${HOME}/Dropbox/org ${HOME}/org/
 
 echo "INFO: ${DATETIME} sync from dropbox to git..."
-rsync -anv ${HOME}/Dropbox/org ${HOME}/Development/private/org-mode/org
+rsync -avz ${HOME}/Dropbox/org ${HOME}/Development/private/org-mode/org
 
 echo "INFO: ${DATETIME} sync from google calendar to dropbox org files..."
 cd ical2org/
