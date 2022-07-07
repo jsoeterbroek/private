f2c7291a600655db9010647f2dd3e78a3e2524b1
Joost Soeterbroek <joost.soeterbroek@gmail.com>
2022-07-07 14:39:33 +0200

    fix rsync

--- org-mode/sync.sh
+++ org-mode/sync.sh
@@ -4,10 +4,10 @@
 DATETIME=`date "+%Y%m%d-%H%M%S"`
 
 echo "INFO: ${DATETIME} sync from dropbox to local org files..."
-rsync -avz ${HOME}/Dropbox/org ${HOME}/org/
+rsync -avz $HOME/Dropbox/org/*.org $HOME/org/
 
 echo "INFO: ${DATETIME} sync from dropbox to git..."
-rsync -avz ${HOME}/Dropbox/org ${HOME}/Development/private/org-mode/org
+rsync -avz $HOME/Dropbox/org/*.org $HOME/Development/private/org-mode/org/
 
 echo "INFO: ${DATETIME} sync from google calendar to dropbox org files..."
 cd ical2org/
